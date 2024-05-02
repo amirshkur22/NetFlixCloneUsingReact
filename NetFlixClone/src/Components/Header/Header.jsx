3040364f4816c9bdccb7b6f96f391dafd7a5bc99
amirshkur22 <amirshkurhu@gmail.com>
2024-05-03 02:31:41 +0300

    secund update

---
+++ NetFlixClone/src/Components/Header/Header.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./header.css";
+import netflix_Logo from "../../Images/NetflixLogo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  return (
+    <div className="headerOuterWrapper">
+      <div className="headerInnerWrapper">
+        <div className="headerLeft">
+          <ul>
+            <li>
+              <a href="/">
+                <img src={netflix_Logo} alt="" width={150} />
+              </a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">TVShow</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">Latest</a>
+            </li>
+            <li>
+              <a href="#">Mylists</a>
+            </li>
+            <li>
+              <a href="#">Browse by Languages</a>
+            </li>
+          </ul>
+        </div>
+        <div className="headerRight">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsIcon />
+            </li>
+
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ NetFlixClone/src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Fotter from "../../Components/Fotter/Fotter";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Fotter />
+    </>
+  );
+};
+
+export default Home;
