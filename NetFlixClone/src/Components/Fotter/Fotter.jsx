207306f738e71a400c4a535b2fa4440c85c67a8a
amirshkur22 <amirshkurhu@gmail.com>
2024-05-03 10:40:18 +0300

    Fotter added

--- NetFlixClone/src/Components/Fotter/Fotter.jsx
+++ NetFlixClone/src/Components/Fotter/Fotter.jsx
@@ -1,12 +1,62 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import "./fotter.css";
 
 const Fotter = () => {
   return (
-    <div>
-      
+    <div className="fotterOutterWrapper">
+      <div className="fotterInnerWrapper">
+        <div className="footerIcons">
+          <li>
+            <FacebookOutlinedIcon />
+          </li>
+          <li>
+            <InstagramIcon />
+          </li>
+          <li>
+            <YouTubeIcon />
+          </li>
+        </div>
+        <div className="fotterData">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+              </div>
+              <div className="serviceCode"><p>Service Code</p></div>
+                  <div className="copyWrite">
+                      &copy; 1997-2024 Netflix, Inc.
+                  </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Fotter
+export default Fotter;
