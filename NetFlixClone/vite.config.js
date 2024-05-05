4aeb40e637b0890bd164cffb89119a7ac5f45bfd
amirshkur22 <amirshkurhu@gmail.com>
2024-05-05 13:50:54 +0300

    Final Commit for Deployment

--- NetFlixClone/vite.config.js
+++ NetFlixClone/vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/NetFlixCloneUsingReact",
   plugins: [react()],
   server: {
     port: 3000,
   },
-  base: "/NetFlixCloneUsingReact",
 });
